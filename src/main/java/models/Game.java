6115bf38da4a5afc96096d650c2686619893d9a6
willmichael <oleemir@gmail.com>
2016-03-08 00:45:17 -0800

    created deck

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -1,7 +1,28 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 /**
  * Created by Michael on 3/8/2016.
  */
 public class Game {
+
+    public java.util.List<Card> deck = new ArrayList<>();
+
+    public void buildDeck() {
+        for(int i = 2; i < 15; i++){
+            deck.add(new Card(i,Suit.Clubs));
+            deck.add(new Card(i,Suit.Hearts));
+            deck.add(new Card(i,Suit.Diamonds));
+            deck.add(new Card(i,Suit.Spades));
+        }
+    }
+
+    public void shuffle() {
+        long seed = System.nanoTime();
+        Collections.shuffle(deck, new Random(seed));
+    }
+
 }
