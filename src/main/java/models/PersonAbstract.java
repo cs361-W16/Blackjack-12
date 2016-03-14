71764cafec44b24677e001c69b488e62113567f7
mtruong080894 <truongm@onid.oregonstate.edu>
2016-03-13 19:06:59 -0700

    Added Hit
    Added Hit(), total score of game, didn't write stay but should be easy
    by checking scores less than #

--- src/main/java/models/PersonAbstract.java
+++ src/main/java/models/PersonAbstract.java
@@ -10,6 +10,7 @@ public abstract class PersonAbstract {
     public int money;
     public int bust;
     public java.util.List<Card> hand = new ArrayList<>();
+    boolean stay;
 
     public void setMoney(int add){
         this.money = money + add;
@@ -34,6 +35,54 @@ public abstract class PersonAbstract {
     public Card getHand(int x){
         return hand.get(x);
     }
+    public int TotalScore()
+    {
+        int sumScore = 0;
+        int temp_score;
+        int ace = 0;
+        // Here ace can be a 1 or 11, we will choose by the sum
+        // Obvious if the sum is 10 or less you want an 11 since you'll be "closer"
+        for (int i = 0; i < hand.size(); i++)
+        {
+            temp_score = getHand(i).getValue();
+            if(temp_score >= 10)
+                temp_score = 10;
+            else if (temp_score <=9)
+                ace++;
+            //when 9 + 11 for 20, break when fails
+            temp_score = 11;
+
+        }
+        if(sumScore > 21)
+            for(int i = 0; i<ace; i++)
+            {
+                temp_score <=10;
+                        break;
+
+            }
+        return sumScore;
+
+
+
+    }
 
     //hit should go here
+    public void hit(ArrayList<Card> deck)
+    {
+        addHand(deck.get(deck.size()-1));
+        deck.remove(deck.size()-1);
+        if(TotalScore() > 21) stay();
+        // if player is greater than 21, stay
+
+
+
+
+
+    }
+    //stay should be written here
+    public void stay()
+    {
+        stay = true;
+
+    }
 }
